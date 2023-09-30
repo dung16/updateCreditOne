706c220089b1fb8b252bf4e896fd0626145d1bb4
dung16 <38211790+dung16@users.noreply.github.com>
2023-09-30 10:31:08 +0700

    clear code

--- client/src/layout/Layout/index.js
+++
@@ -1,8 +0,0 @@
-/** @jsxImportSource @emotion/react */
-import React from "react";
-import tw from "twin.macro";
-function Layout({ children }) {
-  return <div css={tw`p-2`}>{children}</div>;
-}
-
-export default Layout;
